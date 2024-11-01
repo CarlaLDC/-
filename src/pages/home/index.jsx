363faf7ff4db6a93fcc49ee4b57df10f48a8e0ee
Felipe Lima <felipe_limacarvalho@hotmail.com>
2024-10-31 23:33:35 -0300

    symbol system criacao

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,115 @@
+
+import './index.scss'
+
+export default function Syboml() {
+  return (
+    <div className="secao-geral">
+
+      <div className='cabecalho' >
+
+          <img className='logo' src="./assets/image/symbol-system.png" alt="" />
+
+          <div className='link'>
+
+            <h4>Sobre</h4>
+            <h4>Serviços</h4>
+
+          </div>
+
+
+          <button>Login</button>
+
+      </div>
+
+      <img className='image' src="./assets/image/images.png" alt="" />
+      
+      <div className='title' >
+
+        <h1 className='titulo'> Software</h1>
+        <p className='texto'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate alias maxime magnam earum excepturi? Nemo a porro aliquid aut dolore sunt inventore ullam adipisci dolorum optio, reiciendis iste enim magnam!</p>
+
+      </div>
+
+      <div className='cabecalho-dois' >
+
+        <img className='logo' src="./assets/image/symbol-system.png" alt="" />
+
+      </div>
+
+      <div className='talk' >
+
+         <img className='talki' src="./assets/image/talk.jpeg" alt="" />
+
+      </div>
+
+      <hr />
+
+      <div className='secao-dois'>
+          <h3>Design Personalizado</h3>
+          <h3>Preço Fixo</h3>
+          <h3>Implementação rápida</h3>
+
+      </div>
+
+       <button className='proposta' >Peça sua proposta</button>
+
+      <div className='tipos' >
+
+        <img src="./assets/image/mysql.png" alt="" />
+        <img src="./assets/image/jsx.png" alt="" />
+        <img src="./assets/image/bd.png" alt="" />
+        <img src="./assets/image/js.png" alt="" />
+
+      </div>
+
+      <hr />
+
+      <div className='secao-tres'>
+          <h3>Controle completo do seu site em uma única solução</h3>
+
+      </div>
+
+      <div className='fotos' >
+
+        <div className='grupo' >
+
+          <img className='tamanho' src="./assets/image/bote.png" alt="" />
+          <h4>Acesso Remoto</h4>
+
+        </div>
+
+        <div className='grupo' >
+
+          <img className='tamanho' src="./assets/image/config.png" alt="" />
+          <h4>Manutenção Rapida</h4>
+
+        </div>
+
+        <div className='grupo' >
+          <img className='tamanho' src="./assets/image/moeda.png" alt="" />
+          <h4>Custo mensal fixo</h4>
+
+        </div>
+
+      </div>
+
+      <div className='instalacao' >
+
+        <img className='size' src="./assets/image/apple.png" alt="" />
+        <img className='size' src="./assets/image/google.png" alt="" />
+
+      </div>
+
+      <hr />
+
+
+
+
+
+
+
+
+    </div>
+  );
+}
+
---
+++ src/router.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Symbol from './pages/home/index';
+
+
+export default function Navegacao() {
+
+    return (
+
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Symbol />} />
+
+            </Routes>
+        </BrowserRouter >
+
+    )
+
+}
